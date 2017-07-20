44073050d19346bbc892868e4bf526d3171db772
wchen1994 <weigan.chen@outlook.com>
2017-07-20 15:42:34 +1000

    Position clamp on death scene

--- HelloWorld/Assets/UnityChan2D/Demo/Scripts/CameraFollow.cs
+++ HelloWorld/Assets/UnityChan2D/Demo/Scripts/CameraFollow.cs
@@ -13,6 +13,6 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+        transform.position = new Vector3(target.transform.position.x, 5.2f, -10);
 	}
 }
---
+++ HelloWorld/Assets/UnityChan2D/Demo/Scripts/DeathScenePosWrap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScenePosWrap : MonoBehaviour {
+
+    public GameObject player;
+
+    public float leftPos;
+    public float rightPos;
+    public float offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.transform.position.x > rightPos)
+        {
+            player.transform.position -= new Vector3(offset, 0, 0);
+        }
+        if (player.transform.position.x < leftPos)
+        {
+            player.transform.position += new Vector3(offset, 0, 0);
+        }
+		
+	}
+}
